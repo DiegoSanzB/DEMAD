943f9084bd726cea8d3bc305c489c6f49604070d
DiegoSanzB <54013062+DiegoSanzB@users.noreply.github.com>
2019-08-22 20:05:44 -0400

    Add files via upload

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,10 +2,12 @@ extends KinematicBody2D
 
 #Variables
 var motion = Vector2()
-var deadZone = 0.2
+var speedMultiplier = 400
 var dobleSalto = 0
 var controlesConectados = Input.get_connected_joypads().size()
-export var speedMultiplier = 400
+var deadZone = 0.2
+export var control = 0
+var jumpButton = "ui_up" + str(control)
 
 #Constantes
 const UP = Vector2(0, -1)
@@ -17,7 +19,6 @@ func _ready():
 	Input.connect("joy_connection_changed", self,"joy_con_changed")
 	add_to_group('players')
 	
-
 func joy_con_changed(deviceid, isConnected): #primero: identifica coneccion, segundo: identifica si es conexión o desconexión
 	if isConnected:
 		print("Joystick " + str(deviceid) + " connected")
@@ -27,12 +28,12 @@ func joy_con_changed(deviceid, isConnected): #primero: identifica coneccion, seg
 		else:
 			print('Control desconectado')
 #		KinematicBody2D.set_name('Player_%' % deviceid)
-	
+
 #Fisica
 func _physics_process(delta):
 	#Gravedad
 	if Input.get_connected_joypads().size() > 0:
-		var yAxis = Input.get_joy_axis(0,JOY_AXIS_1)
+		var yAxis = Input.get_joy_axis(control,JOY_AXIS_1)
 		if yAxis > 0 and abs(yAxis) > deadZone and motion.y > 0:
 			motion.y += yAxis * 100
 	
@@ -46,17 +47,17 @@ func _physics_process(delta):
 	#print(Input.is_joy_button_pressed(0,JOY_BUTTON_0)) #Debbug: estar apretando A
 	if is_on_floor():
 		dobleSalto = 0
-		if Input.is_action_just_pressed("ui_up"):# or (Input.is_joy_button_pressed(0,JOY_BUTTON_0)):
+		if (Input.is_joy_button_pressed(control,JOY_BUTTON_0)):#Input.is_action_just_pressed("ui_up") or 
 			motion.y = JUMP_HEIGHT
 	if not is_on_floor() and dobleSalto == 0:
-		if Input.is_action_just_pressed("ui_up"):# or Input.is_joy_button_pressed(0,JOY_BUTTON_0):
+		if (Input.is_joy_button_pressed(control,JOY_BUTTON_0)):#Input.is_action_just_pressed("ui_up") or Input.is_joy_button_pressed(control,JOY_BUTTON_0):
 			motion.y = JUMP_HEIGHT
 			dobleSalto =1
 		
 			
 	#Movimiento gorizonttal con joystick
 	if Input.get_connected_joypads().size() > 0:
-		var xAxis = Input.get_joy_axis(0,JOY_AXIS_0)
+		var xAxis = Input.get_joy_axis(control,JOY_AXIS_0)
 		if abs(xAxis) > deadZone:
 			#if  xAxis > 0:
 				motion.x = 100 * delta * speedMultiplier * (xAxis)
@@ -73,19 +74,19 @@ func _physics_process(delta):
 	motion = move_and_slide(motion,UP)
 
 #Crea nuevos player dependiendo de la cantidad de controles
-func multiPlayer():
-	if get_tree().get_nodes_in_group("players").size() < controlesConectados:
-		var player = load("res://KinematicBody2D.gd").new()
-		player.set_name("Player_2")
-		add_child(player)
-	print(has_node("Player_2"))
-	print(get_tree().get_nodes_in_group("players"))
+#func multiPlayer():
+	#if get_tree().get_nodes_in_group("players").size() < controlesConectados:
+		#var player = load("res://KinematicBody2D.gd").new()
+		#player.set_name("Player_2")
+		#add_child(player)
+	#print(has_node("Player_2"))
+	#print(get_tree().get_nodes_in_group("players"))
 
 #Cada frame
 func _process(delta):
 	#Quit game
 	if Input.is_key_pressed(KEY_Q):
 		get_tree().quit()
-	multiPlayer()
+	#multiPlayer()
 
 
