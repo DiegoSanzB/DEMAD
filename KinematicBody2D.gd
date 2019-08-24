cc7d2f62c961e3d518b6a6ab2295b9346116757f
DiegoSanzB <54013062+DiegoSanzB@users.noreply.github.com>
2019-08-23 22:45:01 -0400

    Add files via upload

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,23 +1,32 @@
 extends KinematicBody2D
 
 #Variables
-var motion = Vector2()
-var deadZone = 0.2
-var speedMultiplier = 60
+var motion = Vector2()				#Define hacia donde es el movimiento
+export var speedMultiplier = 400	#Multiplicador de velocidad
+export var acceleration = 0.4		#Aceleracion
+export var airAcceleration = 0.1	#Aceleracion en el aire
+export var friction = 0.3			#Friccion
+export var frictionModifier = 13	#Modificador de Friccion
+var dobleSalto = 0					#Doble Salto
+var controlesConectados = Input.get_connected_joypads().size()	#Cantidad de controles conectados
+var deadZone = 0.2					#Zona de no activacion del joystick
+export var control = 0				#Identificador del control, equivale al orden de conexion
+var jumpButton = "ui_up" + str(control)	#Fail xd
 
 #Constantes
-const UP = Vector2(0, -1)
-const GRAVITY = 20
-const JUMP_HEIGHT = -550
+const UP = Vector2(0, -1)			#Define hacia donde es arriba
+const GRAVITY = 20					#Constante de gravedad
+const JUMP_HEIGHT = -550			#Salto maximo
 
 #Check de control
 func _ready():
-# warning-ignore:return_value_discarded
 	Input.connect("joy_connection_changed", self,"joy_con_changed")
+	add_to_group('players')
 
+#Al conectar un control printea su numero
 func joy_con_changed(deviceid, isConnected): #primero: identifica coneccion, segundo: identifica si es conexión o desconexión
 	if isConnected:
-		print("Joystick" + str(deviceid) + "connected")
+		print("Joystick " + str(deviceid) + " connected")
 		if  Input.is_joy_known(0): #Reconoce algunos tipos de control
 			print('Control reconocido')
 			print(Input.get_joy_name(0) + ' encontrado')
@@ -27,35 +36,54 @@ func joy_con_changed(deviceid, isConnected): #primero: identifica coneccion, seg
 #Fisica
 func _physics_process(delta):
 	#Gravedad
-	motion.y += GRAVITY
+		#Fast Fall. Si el joystick esta hacia abajo aumenta la velocidad de caida
+	if Input.get_connected_joypads().size() > 0:
+		var yAxis = Input.get_joy_axis(control,JOY_AXIS_1)
+		if yAxis > 0 and abs(yAxis) > deadZone and motion.y > 0:
+			motion.y += yAxis * 100
+	
+	if motion.y > 0:			#Gravedad en bajada
+		motion.y += 2 *  GRAVITY
+	else:						#Gravedad en subida
+		motion.y += GRAVITY
 	
 	#Salto
+	#print(dobleSalto) #Debbug: Estado de dobleSlato. 
+	#print(Input.is_joy_button_pressed(0,JOY_BUTTON_0)) #Debbug: estar apretando A
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up") or Input.is_joy_button_pressed(0,JOY_BUTTON_0):
+		dobleSalto = 0
+		if (Input.is_joy_button_pressed(control,JOY_BUTTON_0)):#Input.is_action_just_pressed("ui_up") or 
 			motion.y = JUMP_HEIGHT
+	if not is_on_floor() and dobleSalto == 0:
+		if (Input.is_joy_button_pressed(control,JOY_BUTTON_0)):#Input.is_action_just_pressed("ui_up") or Input.is_joy_button_pressed(control,JOY_BUTTON_0):
+			motion.y = JUMP_HEIGHT
+			dobleSalto =1
+		
+		
 			
-	#Movimiento gorizonttal con joystick
+	#Movimiento horizonttal con joystick
 	if Input.get_connected_joypads().size() > 0:
-		var xAxis = Input.get_joy_axis(0,JOY_AXIS_0)
+		var xAxis = Input.get_joy_axis(control,JOY_AXIS_0)
 		if abs(xAxis) > deadZone:
-			if  xAxis > 0:
-				motion.x = 100 * delta * speedMultiplier * abs(xAxis)
-			elif  xAxis < 0:
-				motion.x = -100 * delta * speedMultiplier * abs(xAxis)
+			if  is_on_floor():
+				motion.x = lerp(motion.x,speedMultiplier*(xAxis),acceleration)
+			else:
+				motion.x = lerp(motion.x,speedMultiplier*(xAxis),airAcceleration)
 		else:
-			motion.x = 0
+			if is_on_floor():
+				motion.x = lerp(motion.x,0,friction)
+			else:
+				motion.x = lerp(motion.x,0,friction/13)
 			
 		#Check de que tecla se esta apretando
-		for i in range(16):
-			if Input.is_joy_button_pressed(0,i):
-				print('Boton ' + str(i) + ' presionado, debiera ser ' + Input.get_joy_button_string(i) )
+		#for i in range(16):
+			#if Input.is_joy_button_pressed(0,i):
+				#print('Boton ' + str(i) + ' presionado, debiera ser ' + Input.get_joy_button_string(i) )
 				
-		
-		
 	motion = move_and_slide(motion,UP)
 
+#Cada frame
+func _process(delta):
 	#Quit game
 	if Input.is_key_pressed(KEY_Q):
 		get_tree().quit()
-
-
